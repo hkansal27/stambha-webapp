7919833bc6d66793211fe87723ec794a3463567a
Himanshu Kansal <himanshu@Himanshus-MacBook-Pro.local>
2020-04-05 10:26:08 +0530

    project see more button functionlity

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,44 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Heading from '../components/Heading'
 import { HashLink as Link } from 'react-router-hash-link';
 
-import { withProjectConsumer } from "../context";
+import { ProjectsContext } from "../context";
 
 
 
-function Project({ context }) {
-    const { handleCategoryChange, selectedProjectList, selectedCategory } = context;
-    let projectList = selectedProjectList ? (selectedProjectList.map(item =>
-        (
-            <div className="col-12 col-md-4 project-image-card">
-                <Link to={`/property/${item.id}`}>
-                    <img src={item.projectSectionImage} alt="project" className="project-image" />
-                </Link>
+export default class Project extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // slug: props.match.params.slug,
+        }
+    }
+    static contextType = ProjectsContext;
+
+    render() {
+
+        const { handleCategoryChange, handleProjectButtonHandler, showSelectedProjectList, selectedCategory, projectButtonText } = this.context;
+
+        let projectList = showSelectedProjectList ? (showSelectedProjectList.map(item =>
+            (
+                <div className="col-12 col-md-4 project-image-card">
+                    <Link to={`/property/${item.id}`}>
+                        <img src={item.projectSectionImage} alt="project" className="project-image" />
+                    </Link>
+                </div>
+            )
+        )) : [];
+        let projectButton = ""
+        if (projectButtonText !== "") {
+            projectButton = (<div className="container project-show-button">
+                <div className="row project-show-button-row">
+                    <button onClick={handleProjectButtonHandler} className="show-button">{projectButtonText}</button>
+                </div>
+            </div>)
+        }
+
+
+        return (
+            <div className="project-section" id="project">
+                <Heading text="Projects" />
+                <div className="category-title">
+                    <ul>
+                        <li style={{ color: selectedCategory === 'interior' ? '#ff4e00' : 'black' }} onClick={() => handleCategoryChange("interior")}>INTERIORS</li>
+                        <li style={{ color: selectedCategory === 'architecture' ? '#ff4e00' : 'black' }} onClick={() => handleCategoryChange("architecture")}>ARCHITECTURE</li>
+                        <li style={{ color: selectedCategory === 'residential' ? '#ff4e00' : 'black' }} onClick={() => handleCategoryChange("residential")}>RESIDENTIAL</li>
+                        <li style={{ color: selectedCategory === 'commercial' ? '#ff4e00' : 'black' }} onClick={() => handleCategoryChange("commercial")}>COMMERCIAL</li>
+                        <li style={{ color: selectedCategory === 'institutional' ? '#ff4e00' : 'black' }} onClick={() => handleCategoryChange("institutional")}>INSTITUTIONAL</li>
+                    </ul>
+                </div>
+
+                <div className="project-image-container container">
+                    <div className="row">
+                        {projectList}
+                    </div>
+                </div>
+                {/* show more or less button */}
+                {projectButton}
             </div>
         )
-    )) : [];
-    console.log(projectList);
-
-    return (
-        <div className="project-section" id="project">
-            <Heading text="Projects" />
-            <div className="category-title">
-                <ul>
-                    <li style={{ color: selectedCategory === 'interior'? '#ff4e00': 'black'}} onClick={() => handleCategoryChange("interior")}>INTERIORS</li>
-                    <li style={{ color: selectedCategory === 'architecture'? '#ff4e00': 'black'}} onClick={() => handleCategoryChange("architecture")}>ARCHITECTURE</li>
-                    <li style={{ color: selectedCategory === 'residential'? '#ff4e00': 'black'}} onClick={() => handleCategoryChange("residential")}>RESIDENTIAL</li>
-                    <li style={{ color: selectedCategory === 'commercial'? '#ff4e00': 'black'}} onClick={() => handleCategoryChange("commercial")}>COMMERCIAL</li>
-                    <li style={{ color: selectedCategory === 'institutional'? '#ff4e00': 'black'}} onClick={() => handleCategoryChange("institutional")}>INSTITUTIONAL</li>
-                </ul>
-            </div>
-
-            <div className="project-image-container container">
-                <div className="row">
-                    {projectList}
-                </div>
-            </div>
-        </div>
-    )
+    }
 }
 
-export default withProjectConsumer(Project);
--- src/context.js
+++ src/context.js
@@ -8,14 +8,16 @@ class ProjectsProvider extends Component {
         projects: [],
         categoryProjects: {},
         selectedProjectList: [],
-        selectedCategory: "interior"
+        showSelectedProjectList: [],
+        selectedCategory: "interior",
+        projectprojectButtonText: ""
     };
 
     componentDidMount() {
         let projects = this.formatProjects(items);
         let categoryProjects = this.getCategoryWiseProject(projects);
         let selectedProjectList = categoryProjects[this.state.selectedCategory];
-        console.log(selectedProjectList);
+        this.setShowProjectList(selectedProjectList);
         this.setState({
             projects,
             categoryProjects,
@@ -23,16 +25,20 @@ class ProjectsProvider extends Component {
         });
     }
 
+    isMobileDevice() {
+        return ((window.innerWidth <= 768));
+    }
+
     formatProjects(items) {
         let tempProjects = items.map(item => {
             let id = item.sys.id;
-            let coverImage =item.images[0] ? item.images[0] : [];
-            let images = item.images.length > 1 ? item.images.slice(1) :  [];
+            let coverImage = item.images[0] ? item.images[0] : [];
+            let images = item.images.length > 1 ? item.images.slice(1) : [];
             let room = { ...item.fields, coverImage, images, id };
             return room;
         });
         return tempProjects;
-    } 
+    }
 
     getCategoryWiseProject(projects) {
         // default category
@@ -47,8 +53,8 @@ class ProjectsProvider extends Component {
         projects.forEach(project => {
             let categoryItem = project.category;
 
-            for(let key in categoryItem) {
-                if(categoryItem[key]) {
+            for (let key in categoryItem) {
+                if (categoryItem[key]) {
                     categoryList[key].push(project);
                 }
             }
@@ -66,15 +72,60 @@ class ProjectsProvider extends Component {
     handleCategoryChange = category => {
         let selectedCategory = category;
         let selectedProjectList = this.state.categoryProjects[category] ? this.state.categoryProjects[category] : [];
+        this.setShowProjectList(selectedProjectList)
         this.setState({
             selectedCategory,
             selectedProjectList: selectedProjectList
         })
     };
 
+    handleProjectButtonHandler = () => {
+        let tempSelectedProjectList;
+        if (this.state.projectButtonText === "See More") {
+            this.setState({
+                projectButtonText: "See Less"
+            })
+           
+            tempSelectedProjectList = this.state.selectedProjectList;
+        } else {
+            this.setState({
+                projectButtonText: "See More"
+            })
+
+            if ((this.state.selectedProjectList.length > 5 && this.isMobileDevice()) || (this.state.selectedProjectList.length > 9 && !this.isMobileDevice())) {
+                let index = this.isMobileDevice() ? 5 : 9;
+                tempSelectedProjectList = this.state.selectedProjectList.slice(0, index);
+            } else {
+                tempSelectedProjectList = this.state.selectedProjectList;
+            }
+        }
+        this.setState({
+            showSelectedProjectList: tempSelectedProjectList
+        })
+    }
+
+    setShowProjectList = (projectList) => {
+        let tempSelectedProjectList = [];
+        if ((projectList.length > 5 && this.isMobileDevice()) || (projectList.length > 9 && !this.isMobileDevice())) {
+            let index = this.isMobileDevice() ? 5 : 9;
+            tempSelectedProjectList = projectList.slice(0, index);
+            this.setState({
+                projectButtonText: "See More"
+            })
+        } else {
+            tempSelectedProjectList = projectList;
+            this.setState({
+                projectButtonText: ""
+            })
+        }
+        this.setState({
+            showSelectedProjectList: tempSelectedProjectList
+        });
+    }
+
     render() {
         return (
-            <ProjectsContext.Provider value={{ ...this.state, getProject: this.getProject, handleCategoryChange: this.handleCategoryChange }} >
+            <ProjectsContext.Provider value={{ ...this.state, getProject: this.getProject, handleProjectButtonHandler: this.handleProjectButtonHandler, handleCategoryChange: this.handleCategoryChange }} >
                 {this.props.children}
             </ProjectsContext.Provider>
         )
